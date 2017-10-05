0cd4f4bd3f511c86a37767c1f1cfca4f0a3ab1d7
kishu.b153@gmail.com <kishu.b153@gmail.com>
2017-10-05 00:32:05 -0400

    componetnts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{name}}</h1>
+  <div> whats app {{name}} </div>
+  <input type = "text" [value]="name" (keyup) = "name = $event.target.value">
+  <br/>
+  <ul>
+  <li>
+  <span>{{customers.id}}</span>
+  </li>
+  </ul>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { name = 'Angular';
+customers = [
+{id:1, name: 'kishore'},
+{id:2, name: 'kishu'},
+{id:3, name: 'b'}
+	 ];
+ }
+
